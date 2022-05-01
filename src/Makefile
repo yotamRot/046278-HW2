ef81b29fcb7257d639687cf44d737f4332be2910
PLIOPS\yotamr <yotamr@pliops.com>
2022-05-02 00:54:28 +0300

    files from moodle

---
+++ src/Makefile
@@ -0,0 +1,30 @@
+DEBUG=0
+
+ifneq ($(DEBUG), 0)
+CFLAGS=-O0 -g -G
+else
+CFLAGS=-O3 -lineinfo
+endif
+
+CFLAGS+=-Xcompiler=-Wall -maxrregcount=32 -arch=sm_75
+CFLAGS+=`pkg-config opencv --cflags --libs`
+# Use to find out shared memory size
+# CFLAGS+=--ptxas-options=-v 
+
+FILES=ex2 hello-shmem
+
+all: $(FILES)
+
+ex2: ex2.o main.o ex2-cpu.o 
+	nvcc --link -L. -lutils $(CFLAGS) $^ -o $@
+hello-shmem: hello-shmem.o
+	nvcc --link $(CFLAGS) $^ -o $@
+
+ex2.o: ex2.cu ex2.h
+main.o: main.cu ex2.h
+
+%.o: %.cu
+	nvcc --compile -dc $< $(CFLAGS) -o $@
+
+clean::
+	rm -f *.o $(FILES)
---
+++ src/ex2.cu
@@ -0,0 +1,120 @@
+#include "ex2.h"
+#include <cuda/atomic>
+
+__device__ void prefix_sum(int arr[], int arr_size) {
+    // TODO complete according to hw1
+}
+
+/**
+ * Perform interpolation on a single image
+ *
+ * @param maps 3D array ([TILES_COUNT][TILES_COUNT][256]) of    
+ *             the tiles’ maps, in global memory.
+ * @param in_img single input image, in global memory.
+ * @param out_img single output buffer, in global memory.
+ */
+__device__
+ void interpolate_device(uchar* maps ,uchar *in_img, uchar* out_img);
+
+__device__
+void process_image(uchar *in, uchar *out, uchar* maps) {
+    // TODO complete according to hw1
+}
+
+__global__
+void process_image_kernel(uchar *in, uchar *out, uchar* maps){
+    process_image(in, out, maps);
+}
+
+class streams_server : public image_processing_server
+{
+private:
+    // TODO define stream server context (memory buffers, streams, etc...)
+
+public:
+    streams_server()
+    {
+        // TODO initialize context (memory buffers, streams, etc...)
+    }
+
+    ~streams_server() override
+    {
+        // TODO free resources allocated in constructor
+    }
+
+    bool enqueue(int img_id, uchar *img_in, uchar *img_out) override
+    {
+        // TODO place memory transfers and kernel invocation in streams if possible.
+        return false;
+    }
+
+    bool dequeue(int *img_id) override
+    {
+        return false;
+
+        // TODO query (don't block) streams for any completed requests.
+        //for ()
+        //{
+            cudaError_t status = cudaStreamQuery(0); // TODO query diffrent stream each iteration
+            switch (status) {
+            case cudaSuccess:
+                // TODO return the img_id of the request that was completed.
+                //*img_id = ...
+                return true;
+            case cudaErrorNotReady:
+                return false;
+            default:
+                CUDA_CHECK(status);
+                return false;
+            }
+        //}
+    }
+};
+
+std::unique_ptr<image_processing_server> create_streams_server()
+{
+    return std::make_unique<streams_server>();
+}
+
+// TODO implement a lock
+// TODO implement a MPMC queue
+// TODO implement the persistent kernel
+// TODO implement a function for calculating the threadblocks count
+
+class queue_server : public image_processing_server
+{
+private:
+    // TODO define queue server context (memory buffers, etc...)
+public:
+    queue_server(int threads)
+    {
+        // TODO initialize host state
+        // TODO launch GPU persistent kernel with given number of threads, and calculated number of threadblocks
+    }
+
+    ~queue_server() override
+    {
+        // TODO free resources allocated in constructor
+    }
+
+    bool enqueue(int img_id, uchar *img_in, uchar *img_out) override
+    {
+        // TODO push new task into queue if possible
+        return false;
+    }
+
+    bool dequeue(int *img_id) override
+    {
+        // TODO query (don't block) the producer-consumer queue for any responses.
+        return false;
+
+        // TODO return the img_id of the request that was completed.
+        //*img_id = ... 
+        return true;
+    }
+};
+
+std::unique_ptr<image_processing_server> create_queues_server(int threads)
+{
+    return std::make_unique<queue_server>(threads);
+}
