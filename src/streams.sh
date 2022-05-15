2dcd1035761b97775d905c672c2628e4dbf99846
yotamRotelman <rotelmanyotam@gmail.com>
2022-05-15 23:06:49 +0300

    fix in script

--- src/streams.sh
+++ src/streams.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-max_load=260000
+max_load=$1
 max_load_cur=$(( $max_load/10 ))
 max_load_max=$(( $max_load*2 ))
+step=$(((  $max_load_max-$max_load_cur )/10 ))
 while [ $max_load_cur -le $max_load_max ]
 do
-   ./ex2 streams $max_load_cur | grep throughput | cut -d " " -f 3
-   max_load_cur=$(( $max_load_cur+10 ))
+   echo "$max_load_cur - $max_load_max"
+   ./ex2 streams $max_load_cur | grep throughput | cut -d " " -f 3 >> out.txt
+   max_load_cur=$(( $max_load_cur+$step ))
 done
